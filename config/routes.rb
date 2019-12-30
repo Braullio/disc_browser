bfba68d11768c6ba0a9c43b329efe50208c0abf4
Braullio Vinnicius <braulliovg@gmail.com>
2019-12-30 11:14:44 -0300

    update(routes) create disc_perfis / disc_history

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@
 
 Rails.application.routes.draw do
   root 'home#index'
+  resources :home, only: %i[index]
+  get 'home/disc_history', to: 'home#disc_history'
+  get 'home/disc_perfis', to: 'home#disc_perfis'
   resources :questionnaire_disc, only: %i[index create show]
 end
