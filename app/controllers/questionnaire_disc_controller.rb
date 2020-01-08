6abba1cd6aef045922b32982211313ea9072e802
Braullio Vinnicius <braulliovg@gmail.com>
2020-01-08 12:52:27 -0300

    code refactor

--- app/controllers/questionnaire_disc_controller.rb
+++ app/controllers/questionnaire_disc_controller.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Manager questionnaire in use
 class QuestionnaireDiscController < ApplicationController
   before_action :questionnaire_params, only: %i[create]
   before_action :report_values, only: %i[show]
@@ -9,42 +12,67 @@ class QuestionnaireDiscController < ApplicationController
   def create
     @questionnaire = QuestionnaireDisc.new(@questionnaire_params)
     if @questionnaire.save
-      flash[:success] = (t 'messages.success.create', value: (t 'controller.board'))
+      flash[:success] = t 'messages.success.create', value: (t 'controller.board')
       redirect_to questionnaire_disc_index_path
     else
-      flash[:danger] = (t 'messages.error.create', value: (t 'controller.board'))
+      flash[:danger] = t 'messages.error.create', value: (t 'controller.board')
       redirect_to root_path
     end
   end
 
   private
+
   def questionnaire_params
-    @questionnaire_params = params.permit(:question01, :question02, :question03,
-                                          :question04, :question05, :question06,
-                                          :question07, :question08, :question09,
-                                          :question10)
+    @questionnaire_params = params.permit(
+      :title, :question01, :question02, :question03,
+      :question04, :question05, :question06, :question07,
+      :question08, :question09, :question10
+    )
   end
 
   def report_values
     @report = QuestionnaireDisc.find(params[:id])
+    sum_question_index0
+    sum_question_index1
+    sum_question_index2
+    sum_question_index3
     report_analytics
   end
 
   def report_analytics
-    @value_max = [sum_question(0), sum_question(1), sum_question(2), sum_question(3)].max
+    @value_max = [
+      @sum_index0, @sum_index1,
+      @sum_index2, @sum_index3
+    ].max
     @profile = []
-    @value_max.equal?(sum_question(0)) ? @profile.push('D') : nil
-    @value_max.equal?(sum_question(1)) ? @profile.push('I') : nil
-    @value_max.equal?(sum_question(2)) ? @profile.push('S') : nil
-    @value_max.equal?(sum_question(3)) ? @profile.push('C') : nil
+    @value_max.equal?(@sum_index0) ? @profile.push('D') : nil
+    @value_max.equal?(@sum_index1) ? @profile.push('I') : nil
+    @value_max.equal?(@sum_index2) ? @profile.push('S') : nil
+    @value_max.equal?(@sum_index3) ? @profile.push('C') : nil
+  end
+
+  def sum_question_index0
+    @sum_index0 = sum_question(0)
+  end
+
+  def sum_question_index1
+    @sum_index1 = sum_question(1)
+  end
+
+  def sum_question_index2
+    @sum_index2 = sum_question(2)
+  end
+
+  def sum_question_index3
+    @sum_index3 = sum_question(3)
   end
 
   def sum_question(number)
-    [convert_array(@report.question01)[number], convert_array(@report.question02)[number],
-    convert_array(@report.question03)[number], convert_array(@report.question04)[number],
-    convert_array(@report.question05)[number], convert_array(@report.question06)[number],
-    convert_array(@report.question07)[number], convert_array(@report.question08)[number],
-    convert_array(@report.question09)[number], convert_array(@report.question10)[number]].sum
+    (1..10).inject(0) do |sum, number_question|
+      number_question.to_i < 10 ? number_question = "0#{number_question}" : nil
+      sum.to_i + @report["question#{number_question}"]
+                 .tr('[] ', '').split(',')[number].to_i
+    end
   end
 
   def convert_array(variable)
