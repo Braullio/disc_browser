a66fedc939612ab4e7ee96062a24c7cc01d1db4c
Braullio Vinnicius <braulliovg@gmail.com>
2020-01-12 22:29:15 -0300

    feat: add route questionnaire history

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :home, only: %i[index]
   get 'home/disc_history', to: 'home#disc_history'
   get 'home/disc_perfis', to: 'home#disc_perfis'
+  get 'questionnaire_disc/history', to: 'questionnaire_disc#history'
   resources :questionnaire_disc, only: %i[index create show]
 end
