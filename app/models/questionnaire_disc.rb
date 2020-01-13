f70330b01e480ac3a0a0330273253cdfbc7b2f4f
Braullio Vinnicius <braulliovg@gmail.com>
2020-01-12 22:35:26 -0300

    refactor: processing questionnaire after create

--- app/models/questionnaire_disc.rb
+++ app/models/questionnaire_disc.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
+# QUestionnaire Disc Model
 class QuestionnaireDisc < ApplicationRecord
+  include Questionnaire
+
+  after_create :calculate_profile
+
+  def calculate_profile
+    profile = Questionnaire::V1::CalculateFormService.run(self)
+    update_attribute(:profile, profile)
+  end
 end
